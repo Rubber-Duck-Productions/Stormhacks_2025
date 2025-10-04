f5c321ce53bdad519902b6c7db7f15d53ad49447
yadav412 <yadavsingh3012@gmail.com>
2025-10-04 14:14:13 -0700

    basic encryption engine

---
+++ encryption.py
@@ -0,0 +1,32 @@
+
+
+Plain = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+Cipher = ["X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]
+
+
+def encryptceaser(message):
+    em=""
+    for i in range(len(message)):
+        for j in range(len(Plain)):
+            if message[i] == Plain[j]:
+                em= em + Cipher[j]
+            elif message[i] == " ":
+                em= em + " "
+    
+    return em
+
+def Dencryptceaser(message): #works are ceaser cipher ecryption (happy accident)
+    
+    em=""
+    for i in range(len(message)):
+        for j in range(len(Plain)):
+            if message[i] == Cipher[j]:
+                em= em + Plain[j]
+            elif message[i] == " ":
+                em= em + " "
+    return em
+
+
+
+
+
